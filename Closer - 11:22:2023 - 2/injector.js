7f25bded698349cbd5f3acfd4da008964e2e7f60
gordonheroa <75142875+gordonheroa@users.noreply.github.com>
2023-11-22 12:34:33 -0500

    Add files via upload

---
+++ Closer - 11:22:2023 - 2/injector.js
@@ -0,0 +1,17 @@
+var injected;
+chrome.runtime.onMessage.addListener(
+  function(request, sender) {
+    injected = request.key;
+  }
+);
+var FormElement = document.getElementsByTagName("form")[0];
+var fields = FormElement.querySelectorAll("textarea");
+fields.forEach(function(item) {
+            var formTitle = item.closest("div[role='listitem']").querySelector("div[role='heading']").firstChild.textContent;
+    if (formTitle.trim() == "codelocation") {
+        item.value = injected;
+        item.setAttribute("data-initial-value", injected);
+        item.setAttribute("badinput", "false");
+        item.parentElement.previousElementSibling.style.display = "none";
+    }
+});
